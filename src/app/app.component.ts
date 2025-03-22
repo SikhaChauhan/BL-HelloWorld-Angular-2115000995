76dbd1700b505b7d20b387f400125c17b0e8d56e
SikhaChauhan <csikha1010@gmail.com>
2025-03-22 05:36:34 +0530

    [Sikha] Add: Added logic to display Hello from BridgeLabz with a user inputted name and display error if exists

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,16 @@ export class AppComponent {
   openBridgeLabz(): void {
     window.open(this.bridgeLabzUrl, '_blank');
   }
+  errorMessage: string = '';
+
+  validateInput(): void {
+    const namePattern = /^[A-Z][a-zA-Z]{2,}$/;
+
+    if (!this.userName.match(namePattern)) {
+      this.errorMessage =
+        'Invalid name! It should start with a capital letter and be at least 3 characters long.';
+    } else {
+      this.errorMessage = '';
+    }
+  }
 }
